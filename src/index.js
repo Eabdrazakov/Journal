d7b36e11715a88c7afafe171dc2ad0e7d5dfe4fb
Padma Patil <abdrazakovermek9@gmail.com>
2023-12-17 12:44:34 -0800

    Install ESlint

--- src/index.js
+++ src/index.js
@@ -1,24 +1,28 @@
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './css/styles.css';
 import Journal from "./journal.js";
 
+
 function reset() {
-    const form = document.querySelector("form");
-    form.reset();
+  const form = document.querySelector("form");
+  form.reset();
 }
 
 
 function Counter(e) {
-    e.preventDefault();
-    const titleInput = document.getElementById("title").value;
-    const descriptionInput = document.getElementById("description").value;
-    const newEntry = new Journal(titleInput, descriptionInput);
-    const wordsOutput = document.querySelector("#wordsOutput");
-    const vowelsOutput = document.querySelector("#vowelsOutput");
-    wordsOutput.innerText = `Number of words: ${newEntry.numberOfWords()}`;
-    vowelsOutput.innerText = `Number of vowels: ${newEntry.numberOfVowels()}`;
-    reset();
+  e.preventDefault();
+  const titleInput = document.getElementById("title").value;
+  const descriptionInput = document.getElementById("description").value;
+  const newEntry = new Journal(titleInput, descriptionInput);
+  const wordsOutput = document.querySelector("#wordsOutput");
+  const vowelsOutput = document.querySelector("#vowelsOutput");
+  wordsOutput.innerText = `Number of words: ${newEntry.numberOfWords()}`;
+  vowelsOutput.innerText = `Number of vowels: ${newEntry.numberOfVowels()}`;
+  reset();
 }
 
 document.addEventListener("DOMContentLoaded", function () {
-    const form = document.querySelector("form");
-    form.addEventListener("submit", Counter);
-})
+  const form = document.querySelector("form");
+  form.addEventListener("submit", Counter);
+});
--- src/journal.js
+++ src/journal.js
@@ -1,35 +1,35 @@
 export default function Journal(title, description) {
-    this.title = title;
-    this.description = description;
+  this.title = title;
+  this.description = description;
 }
 
 Journal.prototype.numberOfWords = function () {
-    if (this.description.trim().length === " ") {
-        return 0;
-    }
-    const words = this.description.split(" ").filter(word => word !== "" && !(/^\d+$/.test(word)));
-    return words.length;
+  if (this.description.trim().length === " ") {
+    return 0;
+  }
+  const words = this.description.split(" ").filter(word => word !== "" && !(/^\d+$/.test(word)));
+  return words.length;
 };
 
 Journal.prototype.numberOfVowels = function () {
-    const vowels = ["a", "e", "i", "o", "u"];
-    let vowelsCount = 0;
-    const numberOfVowels = this.description.split("");
-    numberOfVowels.forEach(function (letter) {
-        if (vowels.includes(letter)) {
-            vowelsCount += 1;
-        }
-    });
-    return vowelsCount;
+  const vowels = ["a", "e", "i", "o", "u"];
+  let vowelsCount = 0;
+  const numberOfVowels = this.description.split("");
+  numberOfVowels.forEach(function (letter) {
+    if (vowels.includes(letter)) {
+      vowelsCount += 1;
+    }
+  });
+  return vowelsCount;
 };
 
 Journal.prototype.getTeaser = function () {
-    const sentence = this.description.split(/[.!?]/);
+  const sentence = this.description.split(/[.!?]/);
 
-    if (sentence.length === 0) {
-        return 0;
-    }
-    const firstSentence = sentence[0].trim();
-    const words = firstSentence.split(/\s+/).slice(0, 8);
-    return words.join(' ');
+  if (sentence.length === 0) {
+    return 0;
+  }
+  const firstSentence = sentence[0].trim();
+  const words = firstSentence.split(/\s+/).slice(0, 8);
+  return words.join(' ');
 };
